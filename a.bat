2b929f52d46ecde25a5c1c567628046cffbcf735
CreateAqwa <123432578+CreateAqwa@users.noreply.github.com>
2026-03-15 10:42:54 +0530

    Add files via upload

---
+++ a.bat
@@ -0,0 +1,2 @@
+start chrome "http://127.0.0.1:9003"
+python manage.py runserver 9003
---
+++ direct python.bat
@@ -0,0 +1,2 @@
+start chrome "http://127.0.0.1:9003"
+C:\Users\Syed1\AppData\Local\Programs\Python\Python310\python.exe manage.py runserver 9003
